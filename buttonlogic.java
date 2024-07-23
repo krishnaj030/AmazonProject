3b0daf338ae508fd13500443636894d4e22c5383
krishna33@gmail.com <siri123@gmail.com>
2024-07-23 10:17:53 +0000

    Revert "submenu has got changes colour"
    This reverts commit 880664e8aa3be2f43f60135ac2f64db2fcf49d05.

--- buttonlogic.java
+++ buttonlogic.java
@@ -2,10 +2,7 @@ this is the line 1
 code modified again.. logic 2
 
 now we are different key method ssh authentication
-<<<<<<< HEAD
 
 This is the final change to the button.
 
 button colour : pink
-=======
->>>>>>> parent of 2ad8813 (final change to the button)
--- submenu.java
+++ submenu.java
@@ -2,16 +2,3 @@ this is submenu logic for developer2.
 logic added by dev2 - logic 1
 
 menu modified by developer1
-
-this is line item 1
-
-
-this is line item 2
-
-
-this is line item 3
-
-
-this is line item 4 
-
-submenu has got some changes - color 
