880664e8aa3be2f43f60135ac2f64db2fcf49d05
krishna33@gmail.com <siri123@gmail.com>
2024-07-23 10:06:27 +0000

    submenu has got changes colour

--- buttonlogic.java
+++ buttonlogic.java
@@ -2,7 +2,10 @@ this is the line 1
 code modified again.. logic 2
 
 now we are different key method ssh authentication
+<<<<<<< HEAD
 
 This is the final change to the button.
 
 button colour : pink
+=======
+>>>>>>> parent of 2ad8813 (final change to the button)
